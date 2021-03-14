3218c8557352e096acc51ab8fdb4a2c7b404bed9
agrwl-prankit <agrwnlprankit77@gmail.com>
2021-03-15 02:07:08 +0530

    passing category_item to category_meals_screen.dart

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -18,7 +18,7 @@ class CategoriesScreen extends StatelessWidget {
             mainAxisSpacing: 20),
         children: DUMMY_CATEGORIES
             .map(
-              (catData) => CategoryItem(catData.title, catData.color),
+              (catData) => CategoryItem(catData.id, catData.title, catData.color),
             )
             .toList(),
       ),
